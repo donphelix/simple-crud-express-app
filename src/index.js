92183128de836a952306b87616ba49dbd30f5e51
Don Phelix <141995407+dondevteam@users.noreply.github.com>
2025-02-19 15:12:32 +0300

    connect to mongodb using mongoose object modeling tool

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const app = express();
 
 app.get('/', (req, res) => {
@@ -6,7 +7,13 @@ app.get('/', (req, res) => {
 });
 
 // Connect to local mongodb
-
+mongoose.connect('mongodb://localhost/pos')
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
 
 // Listen to the server start
 app.listen(3000);
