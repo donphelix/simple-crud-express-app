ec95e5698560c86a2394bf6da544e922ac9acaf4
Don Phelix <141995407+dondevteam@users.noreply.github.com>
2025-02-20 07:12:33 +0300

    include product model and post data to mongo db

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,18 +1,30 @@
-const mongoose = import("mongoose");
+const mongoose = require("mongoose");
 
-const ProductSchema = mongoose.Schema({
-    name: {
-        type: String, required: [true, "Please enter product name"]
-    }, quantity: {
-        type: Number, default: 0, required: true
-    }, price: {
-        type: Number, required: true, default: 0.00
-    }, image: {
-        type: String, required: false
+const ProductSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter product name"],
+        },
+        quantity: {
+            type: Number,
+            default: 0,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+            default: 0.0,
+        },
+        image: {
+            type: String,
+            required: false,
+        },
     },
-}, {
-    timestamps: true
-});
+    {
+        timestamps: true,
+    }
+);
 
 const Product = mongoose.model("Product", ProductSchema);
 module.exports = Product;
